342ffe5f30570c3bf37eecd1db6b6a4e4f5c758d
Trevor Alt <trevorjalt@gmail.com>
2020-09-18 15:34:43 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIEDEX = require('./moviedex.json')
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -51,9 +52,19 @@ function handleGetMovies(req, res) {
 
 app.get('/movie', handleGetMovies)
 
-const PORT=8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`)
+    return 1
 })
 
